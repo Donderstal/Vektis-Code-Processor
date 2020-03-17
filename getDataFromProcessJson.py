35a59f2f17686eaeb5d3b4ba8ffebe9aa2ed7797
Donderstal <daanonderstal@hotmail.com>
2020-03-17 12:03:47 +0100

    filtering out unused standards, codes etc.

--- getDataFromProcessJson.py
+++ getDataFromProcessJson.py
@@ -1,7 +1,7 @@
 import json
 
-Gebruikte_Codelijsten       = []
-Gebruikte_Gegevenselementen = []
+Gebruikte_Codelijsten_ID_List       = []
+Gebruikte_Gegevenselementen_ID_List = []
 
 with open ('./processedData/standaarden.json', 'r') as file:
     json_data = json.load(file)
@@ -12,24 +12,45 @@ with open ('./processedData/standaarden.json', 'r') as file:
                 if 'ID gegevens' in line:
                     elementAlreadyPresent = False
                     
-                    for element in Gebruikte_Gegevenselementen:
+                    for element in Gebruikte_Gegevenselementen_ID_List:
                         if element == line['ID gegevens']:
                             elementAlreadyPresent = True
 
                     if elementAlreadyPresent == False:
-                        Gebruikte_Gegevenselementen.append(line['ID gegevens'])                
+                        Gebruikte_Gegevenselementen_ID_List.append(line['ID gegevens'])                
 
                 if 'Codelijst' in line:
                     if line['Codelijst'] == "":
                         continue
                     else:
                         elementAlreadyPresent = False
-                        for element in Gebruikte_Codelijsten:
+                        for element in Gebruikte_Codelijsten_ID_List:
                             if element == line['Codelijst']:
                                 elementAlreadyPresent = True
 
                         if elementAlreadyPresent == False:
-                            Gebruikte_Codelijsten.append(line['Codelijst'])          
-    
-    print(Gebruikte_Gegevenselementen)
-    print(Gebruikte_Codelijsten)
+                            Gebruikte_Codelijsten_ID_List.append(line['Codelijst'])          
+
+Gebruikte_Codelijsten       = []
+
+with open ('./fullList/code_lijsten.json', 'r') as file:
+    json_data = json.load(file)
+    for codelijst in json_data:
+        for codeId in Gebruikte_Codelijsten_ID_List:
+            if codelijst['ID'] == codeId:
+                Gebruikte_Codelijsten.append(codelijst)
+
+with open('./processedData/code_lijsten.json', 'w') as json_file:
+        json.dump(Gebruikte_Codelijsten, json_file)
+
+Gebruikte_Gegevenselementen = []
+
+with open ('./fullList/gegevens_elementen.json', 'r') as file:
+    json_data = json.load(file)
+    for gegElement in json_data:
+        for gegevensId in Gebruikte_Gegevenselementen_ID_List:
+            if gegElement['ID'] == gegevensId:
+                Gebruikte_Gegevenselementen.append(gegElement)
+
+with open('./processedData/gegevens_elementen.json', 'w') as json_file:
+        json.dump(Gebruikte_Gegevenselementen, json_file)
